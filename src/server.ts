f9a4d234ce1eb4f0ecd96ce454825d75984318b8
williamwjd <william.jose.wjd@gmail.com>
2022-10-16 23:19:15 -0300

    feat: tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import "express-async-errors";
+
+import { Exception } from './errors/Exception';
 
 import { routes } from './router';
 
 const app = express();
+
 app.use(express.json());
 app.use(routes);
+app.use(Exception);
 
 app.listen(3333, () => {
     console.log('Servidor online');
